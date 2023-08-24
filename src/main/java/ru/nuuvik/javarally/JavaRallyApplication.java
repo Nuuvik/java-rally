75b1666eedfe7f6baaee9f75caa0d89b80849de7
Андрей Варначёв <nuuvik@yandex.ru>
2023-08-24 16:20:57 +0300

    Initial commit

---
+++ src/main/java/ru/nuuvik/javarally/JavaRallyApplication.java
@@ -0,0 +1,13 @@
+package ru.nuuvik.javarally;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRallyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRallyApplication.class, args);
+	}
+
+}
